6977189e0f788184020f28c234ddab69cdad1020
toannguyen01 <ductoancntt@gmail.com>
2018-03-18 00:13:19 +0700

    end day 1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, presence: true
+  validates :price, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
+  validate :description_longer_than_title
+
+  def description_longer_than_title
+    return if title.blank? or description.blank?
+    if description.length < title.length
+      errors.add(:description, 'can\'t be shorter than title ')
+    end
+  end
 end
