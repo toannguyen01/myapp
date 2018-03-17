94999075811b3021999d83c87e6a3839d38223fd
toannguyen01 <ductoancntt@gmail.com>
2018-03-17 23:37:34 +0700

    reversibe migration

---
+++ db/migrate/20180317162454_change_products_price.rb
@@ -0,0 +1,11 @@
+class ChangeProductsPrice < ActiveRecord::Migration[5.1]
+
+
+  def up
+    change_column :products, :price, :decimal, precision:10, scale: 2
+  end
+
+  def down
+    change_column :products, :price, :deimal, precision:6, scale:2
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317161454) do
+ActiveRecord::Schema.define(version: 20180317162454) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20180317161454) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal "price", precision: 6, scale: 2
+    t.decimal "price", precision: 10, scale: 2
     t.boolean "published"
     t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
