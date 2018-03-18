5576f763cd2bff78348751848de6ec587de8cebb
toannguyen01 <ductoancntt@gmail.com>
2018-03-19 05:31:08 +0700

    add scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,8 @@ class Product < ActiveRecord::Base
   validates :title, :description, presence: true
   validates_numericality_of :price, greater_than: 0 
   validate :description_longer_than_title
-
+  scope :published, -> { where(published: true)}
+  # scope :description_includes, -> { where('description ILIKE ?', '%ruby' )}
   def description_longer_than_title
     return if title.blank? or description.blank?
     if description.length < title.length
